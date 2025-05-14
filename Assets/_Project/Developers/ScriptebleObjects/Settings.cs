efe6c711946bb83fc22717233b6779d4a24f6f59
JoahvanderSloot <144343776+JoahvanderSloot@users.noreply.github.com>
2025-05-14 12:42:49 +0200

    Seeker + audio buttons

---
+++ Assets/_Project/Developers/ScriptebleObjects/Settings.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Settings.asset", menuName = "Settings", order = 0)]
+public class Settings : ScriptableObject
+{
+    public bool Audio;
+    public bool Music;
+}
--- Assets/_Project/Developers/Scripts/Seeker.cs
+++ Assets/_Project/Developers/Scripts/Seeker.cs
@@ -1,9 +1,11 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Seeker : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] GameObject flashLight;
+    [SerializeField] float detectionRange;
     float realSpeed;
     Transform player;
 
@@ -22,7 +24,9 @@ public class Seeker : MonoBehaviour
 
             transform.position = Vector3.MoveTowards(transform.position, _targetPosition, realSpeed * Time.deltaTime);
 
-            if (Vector3.Distance(transform.position, _targetPosition) > 5f)
+            float _distance = Vector3.Distance(transform.position, _targetPosition);
+
+            if (_distance > 5f)
             {
                 realSpeed = speed * 2;
             }
@@ -31,11 +35,38 @@ public class Seeker : MonoBehaviour
                 realSpeed = speed;
             }
 
-            Vector3 _lookDirection = _targetPosition - transform.position;
-            Quaternion _targetRotation = Quaternion.LookRotation(_lookDirection);
-            flashLight.transform.rotation = Quaternion.Slerp(flashLight.transform.rotation, _targetRotation, Time.deltaTime * 5f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_targetPosition - transform.position), Time.deltaTime * 5f);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_lookDirection), Time.deltaTime * 5f);
+            if (_distance > 0.3f)
+            {
+                float _angle = Mathf.InverseLerp(0f, 10f, _distance);
+                _angle = 2f - _angle;
+                Vector3 _lookDir = transform.forward + Vector3.down * _angle;
+
+                flashLight.transform.rotation = Quaternion.LookRotation(_lookDir);
+            }
+            else
+            {
+                Quaternion targetRotation = Quaternion.Euler(90, 0, 0);
+                flashLight.transform.rotation = Quaternion.Lerp(flashLight.transform.rotation, targetRotation, Time.deltaTime * 5f);
+            }
+
+            KillPlayer();
+        }
+    }
+
+    private void KillPlayer()
+    {
+        RaycastHit _hit;
+        Vector3 _raycastOrigin = transform.position;
+        _raycastOrigin.y = player.position.y;
+
+        if (Physics.Raycast(_raycastOrigin, (player.position - _raycastOrigin).normalized, out _hit, detectionRange))
+        {
+            if (_hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Player hit by raycast!");
+            }
         }
     }
 }
