942b8919726cfd87232c8f1cc09b73e9ed7e1273
JoahvanderSloot <144343776+JoahvanderSloot@users.noreply.github.com>
2025-05-19 08:56:28 +0200

    Main Menu

--- Assets/_Project/Developers/Scripts/Buttons.cs
+++ Assets/_Project/Developers/Scripts/Buttons.cs
@@ -36,4 +36,18 @@ public class Buttons : MonoBehaviour
     {
         SceneManager.LoadScene("Settings");
     }
+
+    public void QuitGame()
+    {
+        #if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+                Debug.Log(this.name + " : " + this.GetType() + " : " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        #endif
+        #if (UNITY_EDITOR)
+                UnityEditor.EditorApplication.isPlaying = false;
+        #elif (UNITY_STANDALONE)
+             Application.Quit();
+        #elif (UNITY_WEBGL)
+             Application.OpenURL("itch url ");
+        #endif
+    }
 }
